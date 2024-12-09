8b0e083b627aee08ef5d9dee0b09ee97fed3cfb5
Isahi.Rea <Isahi.rea@live.com>
2024-12-08 23:32:37 -0600

    Completed Layout

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 let homeCounter = 0
 let guestCounter = 0
+let homeScore = document.getElementById("home-score")
+let guestScore =document.getElementById("guest-score")
 
 function addOneHome() {
-
+    homeCounter+=1
+    homeScore.textContent = homeCounter
 }
 
 function addTwoHome() {
-
+    homeCounter+=2
+    homeScore.textContent = homeCounter
 }
 
 function addThreeHome() {
-
+    homeCounter+=3
+    homeScore.textContent = homeCounter
 }
 
 function addOneGuest() {
-
+    guestCounter+=1
+    guestScore.textContent = guestCounter
 }
 
 function addTwoGuest() {
-
+    guestCounter+=2
+    guestScore.textContent = guestCounter
 }
 
 function addThreeGuest() {
-
+    guestCounter+=3
+    guestScore.textContent = guestCounter
 }
